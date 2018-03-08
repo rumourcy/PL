b8651a4f084bba88a882db692e13e61c290d2374
trierbo <trier@keessi.com>
2018-03-08 23:18:53 +0800

    chpater04

--- chapter04.scm
+++ chapter04.scm
@@ -17,3 +17,17 @@
     (cond
      ((zero? m) n)
      (else (sub1 (－ n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+     ((null? tup) 0)
+     (else
+      (＋ (car tup) (addtup (cdr tup)))))))
+
+(define ＊
+  (lambda (n m)
+    (cond
+     ((zero? m) 0)
+     (else
+      (＋ n (＊ n (sub1 m)))))))
