c0798779a7494cef01fb61380d1373a611e9e1b9
trierbo <trier@keessi.com>
2018-03-14 13:41:30 +0800

    chpater05 finish

--- chapter05.scm
+++ chapter05.scm
@@ -64,3 +64,105 @@
      (else
       (cons (insertL* new old (car l))
 	    (insertL* new old (cdr l)))))))
+
+(define member*
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     ((atom? (car l))
+      (or (eq? (car l) a)
+	  (member* a (cdr l))))
+     (else
+      (or (member* a (car l))
+	  (member* a (cdr l)))))))
+
+(define leftmost
+  (lambda (l)
+    (cond
+     ((atom? (car l)) (car l))
+     (else
+      (leftmost (car l))))))
+
+;; (define eqlist?
+;;   (lambda (l1 l2)
+;;     (cond
+;;      ((and (null? l1) (null? l2)) #t)
+;;      ((and (null? l1) (atom? (car l2))) #f)
+;;      ((null? l1) #f)
+;;      ((and (atom? (car l1)) (null? l2)) #f)
+;;      ((and (atom? (car l1)) (atom? (car l2)))
+;;       (and (eq? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+;;      ((atom? (car l1)) #f)
+;;      ((null? l2) #f)
+;;      ((atom? (car l2)) #f)
+;;      (else
+;;       (and (eqlist? (car l1) (car l2))
+;; 	   (eqlist? (cdr l1) (cdr l2)))))))
+
+;; (define eqlist?
+;;   (lambda (l1 l2)
+;;     (cond
+;;      ((and (null? l1) (null? l2)) #t)
+;;      ((or (null? l1) (null? l2)) #f)
+;;      ((and (atom? (car l1))
+;; 	   (atom? (car l2)))
+;;       (and (eq? (car l1) (car l2))
+;; 	   (eqlist? (cdr l1) (cdr l2))))
+;;      ((or (atom? (car l1)) (atom? (car l2))) #f)
+;;      (else
+;;       (and (eqlist? (car l1) (car l2))
+;; 	   (eqlist? (cdr l1) (cdr l2)))))))
+
+(define equal?
+  (lambda (s1 s2)
+    (cond
+     ((and (atom? s1) (atom? s2))
+      (eq? s1 s2))
+     ((or (atom? s1) (atom? s2)) #f)
+     (else
+      (eqlist? s1 s2)))))
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     (else
+      (and (equal? (car l1) (car l2))
+	   (eqlist? (cdr l1) (cdr l2)))))))
+
+;; (define rember
+;;   (lambda (s l)
+;;     (cond
+;;      ((null? l) '())
+;;      ((atom? (car l))
+;;       (cond
+;;        ((equal? (car l) s) (cdr l))
+;;        (else
+;; 	(cons (car l)
+;; 	      (rember s (cdr l))))))
+;;      (else
+;;       (cond
+;;        ((equal? (car l) s) (cdr l))
+;;        (else
+;; 	(cons (car l)
+;; 	      (rember s (cdr l)))))))))
+
+;; (define rember
+;;   (lambda (s l)
+;;     (cond
+;;      ((null? l) '())
+;;      (else
+;;       (cond
+;;        ((equal? (car l) s) (cdr l))
+;;        (else
+;; 	(cons (car l) (rember s (cdr l)))))))))
+
+(define rember
+  (lambda (s l)
+    (cond
+     ((null? l) '())
+     ((equal? (car l) s) (cdr l))
+     (else
+      (cons (car l) (rember s (cdr l)))))))
+
