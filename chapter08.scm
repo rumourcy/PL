0bda9cb384c5794763d5bf871da156abcdbcfaf3
trierbo <trier@keessi.com>
2018-03-16 00:08:55 +0800

    chapter08 start

---
+++ chapter08.scm
@@ -0,0 +1,82 @@
+;; (define rember-f
+;;   (lambda (test? a l)
+;;     (cond
+;;      ((null? l) '())
+;;      (else
+;;       (cond
+;;        ((test? (car l) a) (cdr l))
+;;        (else
+;; 	(cons (car l)
+;; 	      (rember-f test? a (cdr l)))))))))
+
+(define eq?-c
+  (lambda (a)
+    (lambda (x)
+      (eq? x a))))
+
+(define rember-f
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+       ((null? l) '())
+       ((test? (car l) a) (cdr l))
+       (else
+	(cons (car l) ((rember-f test?) a (cdr l))))))))
+
+(define insertL-f
+  (lambda (test?)
+    (lambda (new old l)
+      (cond
+       ((null? l) '())
+       ((test? (car l) old)
+	(cons new (cons old (cdr l))))
+       (else
+	(cons (car l)
+	      ((insert-f test?) new old (cdr l))))))))
+
+(define insertR-f
+  (lambda (test?)
+    (lambda (new old l)
+      (cond
+       ((null? l) '())
+       ((test? (car l) old)
+	(cons old (cons new (cdr l))))
+       (else
+	(cons (car l)
+	      ((insertR-f test?) new old (cdr l))))))))
+
+(define seqL
+  (lambda (new old l)
+    (cons new (cons old l))))
+
+(define seqR
+  (lambda (new old l)
+    (cons old (cons new l))))
+
+(define insert-g
+  (lambda (test? seq)
+    (lambda (new old l)
+      (cond
+       ((null? l) '())
+       ((test? (car l) old)
+	(seq new old (cdr l)))
+       (else
+	(cons (car l) ((insert-g test? seq) new old (cdr l))))))))
+
+(define insertL (insert-g eq? seqL))
+
+(define insertR (insert-g eq? seqR))
+
+(define seqS
+  (lambda (new old l)
+    (cons new l)))
+
+(define subst (insert-g eq? seqS))
+
+(define seqrem
+  (lambda (new old l)
+    l))
+
+(define rember
+  (lambda (a l)
+    ((insert-g eq? seqrem) #f a l)))
